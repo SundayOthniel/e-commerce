dc15d58eb16c2323ffd394659618da38f009645a
Sunday Othniel <Sundayothniel@outlook.com>
2025-01-18 01:32:27 +0100

    update

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -106,29 +106,23 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # FOR DEPLOYMENT
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=env('DATABASE_URL'),
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env('NAME'),
-        "USER": env('USER'),
-        "PASSWORD": env('PASSWORD'),
-        "HOST": env('HOST'),
-        "PORT": env('PORT'),
-    }
+    "default": dj_database_url.config(
+        default=env('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
-# user= 
-# password=
-# host=
-# port=
-# dbname=
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env('NAME'),
+#         "USER": env('USER'),
+#         "PASSWORD": env('PASSWORD'),
+#         "HOST": env('HOST'),
+#         "PORT": env('PORT'),
+#     }
+# }
 
 
 # Password validation
