3f1ae727f8c891db3db3723e887c4440ad9d265b
Sunday Othniel <Sundayothniel@outlook.com>
2025-01-20 16:19:02 +0100

    removed procfile

--- admin/serializers.py
+++ admin/serializers.py
@@ -15,35 +15,35 @@ class CreateItemSerializer(serializers.ModelSerializer):
         images = validated_data.pop('images')
         
         fuel_type = validated_data.get("fuel_type").capitalize()
-        condition = validated_data.get("condition").capitalize()
-        millage = validated_data.get("millage")
-        transmission = validated_data.get("transmission").capitalize()
-        first_registration = validated_data.get("first_registration").capitalize()
-        engine_power = validated_data.get("engine_power")
-        description = validated_data.get("description").capitalize()
-        interior_material = validated_data.get("interior_material").capitalize()
-        chases_id = validated_data.get("chases_id")
-        category = validated_data.get("category").capitalize()
-        brand = validated_data.get('brand').capitalize()
-        car_model = validated_data.get("car_model").capitalize()
-        drive_type = validated_data.get("drive_type")
-        price =  validated_data.get("price")
+        # condition = validated_data.get("condition").capitalize()
+        # millage = validated_data.get("millage")
+        # transmission = validated_data.get("transmission").capitalize()
+        # first_registration = validated_data.get("first_registration").capitalize()
+        # engine_power = validated_data.get("engine_power")
+        # description = validated_data.get("description").capitalize()
+        # interior_material = validated_data.get("interior_material").capitalize()
+        # chases_id = validated_data.get("chases_id")
+        # category = validated_data.get("category").capitalize()
+        # brand = validated_data.get('brand').capitalize()
+        # car_model = validated_data.get("car_model").capitalize()
+        # drive_type = validated_data.get("drive_type")
+        # price =  validated_data.get("price")
         
         car = Cars.objects.create(
             fuel_type=fuel_type, 
-            condition=condition, 
-            transmission=transmission,
-            first_registration=first_registration,
-            engine_power=engine_power,
-            interior_material=interior_material,
-            chases_id=chases_id,
-            drive_type=drive_type,
-            description=description,
-            category=category,
-            car_model=car_model,
-            price=price,
-            brand=brand,
-            millage=millage
+            # condition=condition, 
+            # transmission=transmission,
+            # first_registration=first_registration,
+            # engine_power=engine_power,
+            # interior_material=interior_material,
+            # chases_id=chases_id,
+            # drive_type=drive_type,
+            # description=description,
+            # category=category,
+            # car_model=car_model,
+            # price=price,
+            # brand=brand,
+            # millage=millage
         )
         
         CarThumbnail.objects.create(car=car, image=images[0])
