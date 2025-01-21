90bc0818739f7829cbd5e47fbdadf324521084bb
Sunday Othniel <Sundayothniel@outlook.com>
2025-01-21 17:45:45 +0100

    removed procfile

--- admin/models.py
+++ admin/models.py
@@ -86,13 +86,13 @@ class Cars(models.Model):
 class CarImage(models.Model):
     car = models.ForeignKey(Cars, related_name='images',
                             on_delete=models.CASCADE)
-    image = models.CharField()
+    image = models.ImageField(upload_to='media/car_images/', max_length=500)
 
     class Meta:
         db_table = 'car_image'
 
 class CarThumbnail(models.Model):
     car = models.OneToOneField(Cars, on_delete=models.CASCADE, related_name='thumbnail')
-    image = models.CharField()
+    image = models.ImageField(upload_to='car_thumbnail', max_length=500)
     class Meta:
         db_table = 'car_thumbnail'
